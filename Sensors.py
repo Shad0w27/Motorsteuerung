75bc103cd862dd2ef5943232da23a01ef3280d73
Shad0w27 <shad0w27.66242007@gmail.com>
2025-05-15 09:24:30 +0200

    Add sensor enable control for line detection in Sensors.py

--- Sensors.py
+++ Sensors.py
@@ -1,10 +1,12 @@
-from gpiozero import LineSensor
+from gpiozero import LineSensor, DigitalOutputDevice
 
+sensorEnable = DigitalOutputDevice(17)
 sensorL = LineSensor(4)
 sensorR = LineSensor(5)
 sensorC = LineSensor(6)
 
-while True:
+sensorEnable.on()
+for i in range(50):
     if sensorL.value == 1:
         print("Line left detected")
     elif sensorR.value == 1:
@@ -13,3 +15,5 @@ while True:
         print("Line in center detected")
     else:
         print("No line detected")
+
+sensorEnable.off()
